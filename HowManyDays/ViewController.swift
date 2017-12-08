051c0e1be744ca3a4c291c3462980a6ce41d678d
Jimson Vedua <jimson@jimsons-macbook-pro.local>
2017-12-07 22:03:55 -0500

    Initial Commit

---
+++ HowManyDays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowManyDays
+//
+//  Created by Jimson Vedua on 12/7/17.
+//  Copyright © 2017 Jimson Vedua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
